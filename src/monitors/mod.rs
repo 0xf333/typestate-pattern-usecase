bdf52a1630691ae36ad289019f24f1428dab9749
0xf333 <0x333@tuta.io>
2024-11-26 12:09:51 -0500

    refact(server): consolidate route exports

--- src/monitors/mod.rs
+++ src/monitors/mod.rs
@@ -5,8 +5,6 @@ pub mod types;
 pub mod unsafe_monitor;
 
 pub use safe::StablecoinMonitor as SafeMonitor;
+pub use states::{Connected, DataFetched, Unconnected};
 pub use types::StablecoinMetrics;
 pub use unsafe_monitor::StablecoinMonitor as UnsafeMonitor;
-
-// Re-exporting the type states for external use
-pub use states::{Connected, DataFetched, Unconnected};
--- src/server/mod.rs
+++ src/server/mod.rs
@@ -2,5 +2,4 @@ pub mod handlers;
 pub mod routes;
 
 pub use handlers::{safe_monitor_handler, unsafe_monitor_handler};
-pub use routes::create_router;
-pub use routes::run_server;
+pub use routes::{create_router, run_server};
